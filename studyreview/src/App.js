957fd61729b8b990f85e62944156ebd61de0984a
neogoolboss <krossjsnet88@gmail.com>
2024-06-12 23:34:26 +0900

    react review

--- studyreview/src/App.js
+++ studyreview/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+let title = '파이널 프로젝트 연습'
+// document.querySelector('h4').innerHTML = title;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color: 'pink', fontSize: '20px'}}>리액트복습</h4>
+      </div>
+        <h4>{title}</h4>
     </div>
   );
 }
