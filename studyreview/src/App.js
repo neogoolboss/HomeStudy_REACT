f1478172a43554517cbb64461d17042c3a21d338
neogoolboss <krossjsnet88@gmail.com>
2024-06-14 22:45:37 +0900

    2024 06 14

--- studyreview/src/App.js
+++ studyreview/src/App.js
@@ -1,15 +1,37 @@
-import './App.css';
+/* eslint-disable */
 
-let title = '파이널 프로젝트 연습'
+import './App.css';
+import { useState } from 'react';
+
 // document.querySelector('h4').innerHTML = title;
 
 function App() {
+
+  let [boardTitle, setBoardTitle] = useState(['스프링부트', '리액트', '플러터']);
+  let [goodNumber, setGoodNumber] = useState(0);
+
+  // let num = [1, 2];
+  // let a = num[0];
+  // let c = num[1];
+  // let [a, c] = [1, 2];
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4 style={{color: 'pink', fontSize: '20px'}}>리액트복습</h4>
       </div>
-        <h4>{title}</h4>
+      <div className="list">
+        <h4>{ boardTitle[0] } <span onClick={() => { setGoodNumber(goodNumber+1)}}>👌</span> {goodNumber}</h4>
+        <p>6월 14일 금요일</p>
+      </div>
+      <div className="list">
+        <h4>{ boardTitle[1]}<span onClick={() => { setGoodNumber(goodNumber+1)}}>👌</span> {goodNumber}</h4>
+        <p>6월 14일 금요일</p>
+      </div>
+      <div className="list">
+        <h4>{ boardTitle[2] }<span onClick={() => { setGoodNumber(goodNumber+1)}}>👌</span> {goodNumber}</h4>
+        <p>6월 14일 금요일</p>
+      </div>
     </div>
   );
 }
